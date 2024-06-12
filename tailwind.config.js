b90a9694e93b53a827c9e39d24f3c0fb06b8854c
Michal Cudzich <michal.cudzich@gmail.com>
2024-06-12 16:39:06 -0500

    Install sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['"./src/**/*.{html,js,vue}"'],
+  content: [
+      "./index.html",
+      "./src/**/*.{vue,js}"
+  ],
   theme: {
     extend: {},
   },
