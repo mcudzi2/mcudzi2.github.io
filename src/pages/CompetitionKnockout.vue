9c4c11d8e1acfc54764e7abd2d688658ced31759
Michal Cudzich <michal.cudzich@gmail.com>
2024-06-13 21:03:46 -0500

    Knockout bracket placeholder

--- src/pages/CompetitionKnockout.vue
+++ src/pages/CompetitionKnockout.vue
@@ -3,6 +3,11 @@
     Knockout Bracket
   </h3>
   <p class="italics text-center">Enter scores for the matches below. The bracket will automatically complete based on results.</p>
+  <div class="h-60 w-full relative bg-amber-200 border-2 border-amber-400 rounded-xl">
+    <h3 class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-neutral-700 italic">
+      Knockout Bracket Coming Soon!
+    </h3>
+  </div>
   <div class="w-full text-center space-y-2">
     <button
         type="button"
