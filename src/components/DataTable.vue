cd28b042a7f4656af3063cfa63beae04ddeb32c0
Michal Cudzich <michal.cudzich@gmail.com>
2025-02-07 17:36:25 -0600

    Implemented Virtual Scroll on DataTable component. Updated Pokedex to use virtual scroll.

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,72 +1,96 @@
 <template>
-  <table class="w-full relative">
-    <thead :class="{ 'fixed-headers': fixHeaders }">
-      <tr :class="headerClasses">
-        <th
-          v-for="header in headers"
-          :key="header.value"
-          :class="header.headerCellClasses || headerCellClasses(header)"
-        >
-          <div
-            class="flex flex-row items-center gap-x-0.5 cursor-pointer"
-            :class="header.headerContentClasses || headerContentClasses(header)"
-            @click="toggleSort(header.sortBy || header.value, header.value)"
+  <div ref="tableContainer">
+    <table
+      class="w-full relative"
+      :class="tableClasses"
+      :style="tableStyle"
+    >
+      <thead :class="{ 'fixed-headers': fixHeaders }">
+        <tr :class="headerClasses">
+          <th
+            v-for="header in headers"
+            :key="header.value"
+            :class="header.headerCellClasses || headerCellClasses(header)"
           >
-            <slot
-              :name="`header-${header.value}`"
-              v-bind="header"
+            <div
+              class="flex flex-row items-center gap-x-0.5 cursor-pointer"
+              :class="header.headerContentClasses || headerContentClasses(header)"
+              @click="toggleSort(header.sortBy || header.value, header.value)"
             >
-              <h5 v-text="header.name" />
-            </slot>
-            <SvgIcon
-              name="caret"
-              :rotate="sortBy.dir === 'asc' ? 0 : 180"
-              class="sort-icon"
-              :class="{ '!opacity-100': sortBy.header === header.value }"
-            />
-          </div>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in sortedItemsToDisplay"
-        :key="item[itemTrackBy]"
-        :class="item.rowClasses || rowClasses(item)"
-      >
-        <td
-          v-for="header in headers"
-          :key="`${item[itemTrackBy]}-${header.value}`"
-          :class="item.rowCellClasses || rowCellClasses(item, header)"
+              <slot
+                :name="`header-${header.value}`"
+                v-bind="header"
+              >
+                <h5 v-text="header.name" />
+              </slot>
+              <SvgIcon
+                name="caret"
+                :rotate="sortBy.dir === 'asc' ? 0 : 180"
+                class="sort-icon"
+                :class="{ '!opacity-100': sortBy.header === header.value }"
+              />
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody ref="tableBody">
+        <tr
+          v-if="useVirtualScroll"
+          :style="{ height: `${prePaddingRenderHeight}px` }"
         >
-          <div
-            class="flex flex-col"
-            :class="header.rowContentClasses || rowContentClasses(item, header)"
+          <td
+            colspan="100%"
+            class="!p-0 !m-0"
+          />
+        </tr>
+        <tr
+          v-for="item in itemsToDisplay"
+          :key="item[itemTrackBy]"
+          :class="item.rowClasses || rowClasses(item)"
+        >
+          <td
+            v-for="header in headers"
+            :key="`${item[itemTrackBy]}-${header.value}`"
+            :class="item.rowCellClasses || rowCellClasses(item, header)"
           >
-            <slot
-              :name="`item-${header.value}`"
-              v-bind="item"
+            <div
+              class="flex flex-col"
+              :class="header.rowContentClasses || rowContentClasses(item, header)"
             >
-              <p
-               v-if="!header.allowHTML"
-               v-text="get(item, header?.displayAs || header.value)"
-              />
-              <div
-                v-else
-                v-html="get(item, header?.displayAs || header.value)"
-              />
-            </slot>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+              <slot
+                :name="`item-${header.value}`"
+                v-bind="item"
+              >
+                <p
+                 v-if="!header.allowHTML"
+                 v-text="get(item, header?.displayAs || header.value)"
+                />
+                <div
+                  v-else
+                  v-html="get(item, header?.displayAs || header.value)"
+                />
+              </slot>
+            </div>
+          </td>
+        </tr>
+        <tr
+          v-if="useVirtualScroll"
+          :style="{ height: `${postPaddingRenderHeight}px` }"
+        >
+          <td
+            colspan="100%"
+            class="!p-0 !m-0"
+          />
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
 import SvgIcon from "@/components/SvgIcon.vue";
-import { computed } from "vue";
-import { get, clone } from 'lodash';
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
+import { get, debounce } from 'lodash';
 
 const sortBy = defineModel('sortBy', {
   type: Object,
@@ -107,6 +131,14 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  tableClasses: {
+    type: [String, Array, Object],
+    default: '',
+  },
+  tableStyle: {
+    type: [String, Array, Object],
+    default: '',
+  },
   headerClasses: {
     type: [String, Array, Object],
     default: ''
@@ -131,6 +163,18 @@ const props = defineProps({
     type: Function,
     default: () => (() => {}),
   },
+  useVirtualScroll: {
+    type: Boolean,
+    default: false,
+  },
+  rowHeight: {
+    type: [Number, Function],
+    default: 0,
+  },
+  paddingItemCount: {
+    type: Number,
+    default: 3,
+  },
 });
 
 function compareStringToSearchTerm(string) {
@@ -154,7 +198,7 @@ function propertyMatchesSearchTerm(item, field) {
   }
 }
 
-const itemsToDisplay = computed(() => {
+const searchedItems = computed(() => {
   if (!props.searchTerm?.length) {
     return props.items;
   }
@@ -196,15 +240,15 @@ function compareItemsByField(itemA, itemB, field) {
   }
 }
 
-const sortedItemsToDisplay = computed(() => {
+const sortedItems = computed(() => {
   if (!sortBy.value.key?.length) {
-    return itemsToDisplay.value;
+    return searchedItems.value;
   }
 
   const sortKeys = Array.isArray(sortBy.value.key)
     ? sortBy.value.key
     : [sortBy.value.key];
-  return itemsToDisplay.value.sort((itemA, itemB) => {
+  return searchedItems.value.sort((itemA, itemB) => {
     let sortValue = 0;
     for (let field of sortKeys) {
       sortValue = compareItemsByField(itemA, itemB, field);
@@ -214,7 +258,7 @@ const sortedItemsToDisplay = computed(() => {
     }
     return sortValue;
   })
-})
+});
 
 function toggleSort(headerKey, headerValue) {
   if (headerValue !== sortBy.value.header) {
@@ -227,6 +271,82 @@ function toggleSort(headerKey, headerValue) {
     sortBy.value.dir = sortBy.value.dir === 'asc' ? 'desc' : 'asc';
   }
 }
+
+//Virtual Scroll
+const tableContainer = ref();
+const tableHeight = ref(0);
+const tableScrollTop = ref(0);
+const itemsWithPositions = computed(() => {
+  if (!props.useVirtualScroll || typeof props.rowHeight === 'number') {
+    return sortedItems.value;
+  }
+
+  return sortedItems.value.map(item => ({ ...item, position: props.rowHeight(item) }));
+});
+const renderStartIndex = computed(() => {
+  if (!props.useVirtualScroll) {
+    return 0;
+  }
+
+  if (typeof props.rowHeight === 'number') {
+    return Math.max(Math.floor(tableScrollTop.value / props.rowHeight) - props.paddingItemCount, 0);
+  }
+
+  // TODO: Implement dynamic row height virtual scroll
+});
+const itemsToRenderCount = computed(() => {
+  if (!props.useVirtualScroll) {
+    return sortedItems.value.length;
+  }
+
+  if (typeof props.rowHeight === 'number') {
+    const itemCount = Math.ceil(tableHeight.value / props.rowHeight) + (2 * props.paddingItemCount);
+    return renderStartIndex.value + itemCount > itemsWithPositions.value.length
+      ? itemsWithPositions.value.length - renderStartIndex.value
+      : itemCount;
+  }
+
+  // TODO: Implement dynamic row height virtual scroll
+});
+const prePaddingRenderHeight = computed(() => {
+  return (typeof props.rowHeight === 'number')
+      ? props.rowHeight * renderStartIndex.value
+      : itemsWithPositions.value[renderStartIndex.value].position;
+})
+const postPaddingRenderHeight = computed(() => {
+  return (typeof props.rowHeight === 'number')
+      ? (props.rowHeight * itemsWithPositions.value.length - props.rowHeight * itemsToRenderCount.value - prePaddingRenderHeight.value)
+      : itemsWithPositions.value[renderStartIndex.value].position;
+})
+const itemsToDisplay = computed(() => {
+  return props.useVirtualScroll
+    ? itemsWithPositions.value.slice(renderStartIndex.value, renderStartIndex.value + itemsToRenderCount.value)
+    : itemsWithPositions.value;
+});
+
+const handleScroll = debounce(() => {
+  if (!props.useVirtualScroll) {
+    return;
+  }
+  tableScrollTop.value = tableContainer.value.scrollTop;
+}, );
+const handleResize = debounce(() => {
+  if (!props.useVirtualScroll) {
+    return;
+  }
+  tableHeight.value = tableContainer.value.clientHeight;
+}, );
+
+onMounted(() => {
+  tableHeight.value = tableContainer?.value?.clientHeight || 0;
+  tableScrollTop.value = tableContainer?.value?.scrollTop || 0;
+  window.addEventListener('resize', handleResize);
+  tableContainer.value.addEventListener('scroll', handleScroll);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize);
+  tableContainer.value.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <style scoped lang="scss">
